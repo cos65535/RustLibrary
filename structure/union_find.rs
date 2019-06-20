89438fc38b7ffcadd7d493157c19dca2b8af6d72
MORI Shingo <cos@kmc.gr.jp>
2019-06-21 00:30:27 +0900

    add pub

--- structure/union_find.rs
+++ structure/union_find.rs
@@ -1,35 +1,42 @@
 #[allow(dead_code)]
-struct UnionFind {
+#[derive(Clone, Debug)]
+pub struct UnionFind {
     parent: Vec<i32>,
 }
 
 #[allow(dead_code)]
 impl UnionFind {
-    fn new(n: usize) -> UnionFind {
-        UnionFind { parent: vec![-1; n] }
+    pub fn new(n: usize) -> UnionFind {
+        UnionFind {
+            parent: vec![-1; n],
+        }
     }
-    fn root(&mut self, x: usize) -> usize {
+    pub fn root(&mut self, x: usize) -> usize {
         let p = self.parent[x];
         return if p < 0 {
             x
         } else {
             self.parent[x] = self.root(p as usize) as i32;
             self.parent[x] as usize
-        }
+        };
     }
-    fn union_set(&mut self, x: usize, y: usize) -> bool {
+    pub fn union_set(&mut self, x: usize, y: usize) -> bool {
         let mut x = self.root(x);
         let mut y = self.root(y);
-        if x == y { return false; }
-        if self.parent[y] < self.parent[x] { std::mem::swap(&mut x, &mut y); }
+        if x == y {
+            return false;
+        }
+        if self.parent[y] < self.parent[x] {
+            std::mem::swap(&mut x, &mut y);
+        }
         self.parent[x] += self.parent[y];
         self.parent[y] = x as i32;
         true
     }
-    fn find_set(&mut self, x: usize, y: usize) -> bool {
+    pub fn find_set(&mut self, x: usize, y: usize) -> bool {
         self.root(x) == self.root(y)
     }
-    fn size(&mut self, x: usize) -> usize {
+    pub fn size(&mut self, x: usize) -> usize {
         let r = self.root(x);
         -self.parent[r] as usize
     }
